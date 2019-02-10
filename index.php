cb010f0f86be5ecb4a40e901a9881487b45d620c
Kerry Finnegan <Finnegan-K6@ulster.ac.uk>
2019-02-10 11:01:25 +0000

    fixing live chat issues

--- index.php
+++ index.php
@@ -6,6 +6,11 @@ include 'db.php';
 	<head>
 		<title>Chat System in PHP</title>
 	<link rel="stylesheet" href="style.css" media="all"/>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 	<script>
 		function ajax(){
 
@@ -24,18 +29,69 @@ include 'db.php';
 		}
 		setInterval(function(){ajax()},1000);
 	</script>
+	<style>
+	*{
+		padding:0;
+		margin:0;
+		border:0;
+	}
+
+	body {
+		background:black;
+	}
+
+	#container {
+		width:40%;
+		background:white;
+		margin: 0 auto;
+		padding:20px;
+
+	}
+	#chat_box {
+		width:90%;
+		height:400px;
+
+	}
+	#chat_data {
+		width:100%;
+		padding:5px;
+		margin-bottom:5px;
+		border-bottom:1px solid silver;
+		font-weight:bold;
+		overflow-y:scroll;
+	}
+	input[type='text']{
+		width:100%;
+		height:40px;
+		border:1px solid gray;
+		border-radius:5px;
+	}
+	input[type='submit']{
+		width:100%;
+		height:40px;
+		border:1px solid gray;
+		border-radius:5px;
+	}
+	textarea{
+		width:100%;
+		height:40px;
+		border:1px solid gray;
+		border-radius:5px;
+	}
+
+	</style>
 	</head>
 
 <body onload="ajax();">
 
 <div id="container">
 		<div id="chat_box">
-		<div id="chat"></div>
+		<div id="chat_data"></div>
 		</div>
 		<form method="post" action="index.php">
 		<input type="text" name="name" placeholder="enter name"/>
 		<textarea name="msg" placeholder="enter message"></textarea>
-		<input type="submit" name="submit" value="Send it"/>
+		<button type="button" class="btn">Send</button>
 
 		</form>
 		<?php
