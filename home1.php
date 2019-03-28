29695069876525c685844ae7d40a07dbbf03dbc4
Kerry Finnegan <Finnegan-K6@ulster.ac.uk>
2019-03-28 20:09:28 +0000

    Implementing login feature

--- home1.php
+++ home1.php
@@ -29,7 +29,7 @@
           <a class="nav-link" href="#">Software</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="login.php">Login</a>
+          <a class="nav-link" href="loginForm.php">Login</a>
         </li>
         <li class="nav-item active">
           <a class="nav-link" href="index.php">Live Chat</a>
--- login.php
+++ login.php
@@ -1,31 +1,28 @@
 <?php
-// Initialize the session
-session_start();
-
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
+session_start(); // Starting Session
+$error = ''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+  if (empty($_POST['username']) || empty($_POST['password'])) {
+    $error = "Username or Password is invalid";
+  }
+  else{
+    // Define $username and $password
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    // mysqli_connect() function opens a new connection to the MySQL server.
+    $conn = mysqli_connect("localhost", "root", "", "company");
+    // SQL query to fetch information of registerd users and finds user match.
+    $query = "SELECT username, password from login where username=? AND password=? LIMIT 1";
+    // To protect MySQL injection for Security purpose
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $stmt->bind_result($username, $password);
+    $stmt->store_result();
+    if($stmt->fetch()) //fetching the contents of the row {
+      $_SESSION['login_user'] = $username; // Initializing Session
+    header("location: profile.php"); // Redirecting To Profile Page
+  }
+  mysqli_close($conn); // Closing Connection
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
-</head>
-<body>
-    <div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
-    </div>
-    <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
-    </p>
-</body>
-</html>
